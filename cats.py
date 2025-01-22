01654c629d14b6f991b8595b406de9c3cad4cd2e
Sophiya-ai <sophiyabr@gmail.com>
2025-01-22 12:10:52 +0200

    def get_cat_breeds

--- cats.py
+++ cats.py
@@ -4,10 +4,18 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 import random
 from config import TOKEN_CW, API_CAT
+import requests
 
 bot = Bot(token=TOKEN_CW)
 dp = Dispatcher()
 
+def get_cat_breeds():
+    url = 'https://api.thecatapi.com/v1/images/search?breed_ids=%7Bbreed.id%7D'
+    headers = {'x-api-key' : API_CAT}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def get_cat_image_by_breed(breed_id):
 
 
 async def main():
