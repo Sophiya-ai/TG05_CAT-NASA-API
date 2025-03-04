833ae32d11997b4d7321101f3daeb0c8eca1db91
Sophiya <sophiyabr@gmail.com>
2025-03-04 12:58:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 TOKEN_CW = 'your-bot-token'
-API_CAT_KEY = 'live_TnGjXlhO7dBXV7jiaFo8TX3qoFSxIiaJvDXUZuIVewz7dPg93Hx8p2zqyd6D7b8u'
-API_NASA_KEY = '9jBc0DWaIwV7fAMDBIjgXb7DN7MnUBotxnAJXpW3'
+API_CAT_KEY = 'your-API-key' # https://thecatapi.com/
+API_NASA_KEY = 'your-API-key' # https://api.nasa.gov/
 
 TOKEN = 'your-bot-token'
-API_GIF = 'AIzaSyB8AkrNbs5zpgaPDNIaBK-VnUbuMgi-ZuY' #https://tenor.com/gifapi
+API_GIF = 'your-API-key' # https://tenor.com/gifapi
 
 
