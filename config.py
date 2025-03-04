09f0de9a99a954d5c905eef9cdfd30b3bc27fe91
Sophiya <sophiyabr@gmail.com>
2025-03-04 12:56:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-TOKEN_CW = '8128353058:AAHxAt8ixolikuac8WfLxCMC67hNWhZe-Ts'
+TOKEN_CW = 'your-bot-token'
 API_CAT_KEY = 'live_TnGjXlhO7dBXV7jiaFo8TX3qoFSxIiaJvDXUZuIVewz7dPg93Hx8p2zqyd6D7b8u'
 API_NASA_KEY = '9jBc0DWaIwV7fAMDBIjgXb7DN7MnUBotxnAJXpW3'
 
-TOKEN = '7868972083:AAHjnClkScE-lIJvTrzR3quHuiyXpPgy2nc'
+TOKEN = 'your-bot-token'
 API_GIF = 'AIzaSyB8AkrNbs5zpgaPDNIaBK-VnUbuMgi-ZuY' #https://tenor.com/gifapi
 
 
